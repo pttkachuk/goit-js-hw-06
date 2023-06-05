027cee6a6be919b29f3a1cbb4c68cc185a30d0a9
pttkachuk <petrotkachuk6@gmail.com>
2023-06-05 18:28:53 +0200

    done task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChangeColor = document.querySelector('.change-color');
+const colorBg = document.querySelector('.color');
+
+btnChangeColor.addEventListener('click', changeBgColor);
+
+function changeBgColor(color) {
+  const randomColor = getRandomHexColor();
+  document.body.style.background = randomColor;
+  colorBg.textContent = randomColor;
+}
+
+
