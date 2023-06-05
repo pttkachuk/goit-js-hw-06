e24c656d5f5b986d530a0bab4f7e6b7a20d22e55
pttkachuk <petrotkachuk6@gmail.com>
2023-06-05 18:28:42 +0200

    done task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
+const dataForm = document.querySelector('.login-form');
 
+dataForm.addEventListener('submit', handleDataFormSubmit);
+
+function handleDataFormSubmit(e) {
+    e.preventDefault();
+    const { email, password } = e.currentTarget;
+    if (email.value === '' || password.value === '') {
+        return alert("Please fill in all the fields!")
+    }
+    e.currentTarget.reset()
+}
